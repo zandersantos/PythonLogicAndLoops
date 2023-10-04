a9f47d4bd080a256a234143c7a8b8b504684e7e3
Zander Santos <zsantos@RRC.ca>
2023-10-04 17:56:04 -0500

    Defined a list of all transactions, declared a variable to store the balance and printed off the beginning interface under bank_transactions

--- bank_transactions.py
+++ bank_transactions.py
@@ -4,3 +4,23 @@ Author: Zander Santos
 Date: October 2, 2023
 Usage:
 """
+# import section
+import random
+
+transaction_options = ["D", "W", "Q"]
+user_balance = float(random.randint(-1000,10000))
+
+print(user_balance)
+
+interface_break = "*" * 40
+
+# print the beginning interface
+print(interface_break)
+print("PIXEL RIVER FINANCIAL".center(40," "))
+print("ATM Simulator".center(40," "))
+print(f"Your current balance is: ${user_balance:,.2f}".center(40," "))
+print(" ")
+print("Deposit: D".center(40," "))
+print("Withdraw: W".center(40," "))
+print("To Quit: Q".center(40," "))
+print(interface_break)
