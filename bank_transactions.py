a6f6e46e1e2cd05cdbbf1b7073e41c218572b7b1
Zander Santos <zsantos@RRC.ca>
2023-10-04 19:11:22 -0500

    Incorporated a loop which repeated the code starting at the display of the ATM options and terminates when the user enters Q

--- bank_transactions.py
+++ bank_transactions.py
@@ -6,44 +6,49 @@ Usage:
 """
 # import section
 import random
+import os
+from time import sleep
 
 transaction_options = ["D", "W", "Q"]
 user_balance = float(random.randint(-1000,10000))
 
-print(user_balance)
-
 interface_break = "*" * 40
+user_input = ""
 
-# print the beginning interface
-print(interface_break)
-print("PIXEL RIVER FINANCIAL".center(40," "))
-print("ATM Simulator".center(40," "))
-print(f"Your current balance is: ${user_balance:,.2f}".center(40," "))
-print(" ")
-print("Deposit: D".center(40," "))
-print("Withdraw: W".center(40," "))
-print("To Quit: Q".center(40," "))
-print(interface_break)
-
-user_input = (input("Enter your selection: "))
-
-if not user_input in transaction_options:
-    print("INVALID SELECTION".center(40," "))
+while not user_input == "Q":
+    # print the beginning interface
     print(interface_break)
+    print("PIXEL RIVER FINANCIAL".center(40," "))
+    print("ATM Simulator".center(40," "))
+    print(f"Your current balance is: ${user_balance:,.2f}".center(40," "))
+    print(" ")
+    print("Deposit: D".center(40," "))
+    print("Withdraw: W".center(40," "))
+    print("To Quit: Q".center(40," "))
+    print(interface_break)
+
+    user_input = (input("Enter your selection: "))
     
-elif user_input == "D":
-    enter_transaction = (float(input("Enter amount of transaction:")))
-    user_balance = user_balance + enter_transaction
-    
-    print(interface_break)
-    print(f"Your current balance is: ${user_balance:,.2f}".center(40, " "))
-    print(interface_break)
-
-elif user_input == "W":
-    enter_transaction = (float(input("Enter amount of transaction:")))
-    if enter_transaction > user_balance:
-        print("INSUFFICIENT FUNDS".center(40," "))
-    else:
-        user_balance = user_balance - enter_transaction
+    if not user_input in transaction_options:
+        print("INVALID SELECTION".center(40," "))
+        print(interface_break)
+        
+    elif user_input == "D":
+        enter_transaction = (float(input("Enter amount of transaction:")))
+        user_balance = user_balance + enter_transaction
+        
+        print(interface_break)
         print(f"Your current balance is: ${user_balance:,.2f}".center(40, " "))
+        print(interface_break)
+
+    elif user_input == "W":
+        enter_transaction = (float(input("Enter amount of transaction:")))
+        if enter_transaction > user_balance:
+            print("INSUFFICIENT FUNDS".center(40," "))
+        else:
+            user_balance = user_balance - enter_transaction
+            print(f"Your current balance is: ${user_balance:,.2f}".center(40, " "))
+
+    sleep(3)
+    os.system('cls' if os.name == 'nt' else 'clear')
 
