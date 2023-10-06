833d4e00f81a81a2882936fb295c6ae1ba4fa011
Zander Santos <zsantos@RRC.ca>
2023-10-06 12:09:01 -0500

    Created a dictionary and put the information inside the account_balances into it. Then displayed current balances

--- interest_update.py
+++ interest_update.py
@@ -4,3 +4,15 @@ Author: Zander Santos
 Date: October 2, 2023
 Usage:
 """
+
+import pprint
+
+account_balances = {}
+
+
+with open("account_balances.txt","r") as file:
+    for row in file:
+        key, value = row.strip().split('|')
+        account_balances [key] = float(value)
+        
+pprint.pprint(account_balances)
